1127a8d095acce939722ad576121a47a3d115621
Álvaro Piza <59285436+alvarotoledopiza@users.noreply.github.com>
2021-12-16 05:33:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+ano_nascimento = input('Insira sua data de nascimento: ')
+print(type(ano_nascimento))
+ano_atual = input('Insira o ano atual: ')
+print(type(ano_atual))
+idade = int(ano_atual) - int(ano_nascimento)
+print(type(idade))
+print(idade)
+
